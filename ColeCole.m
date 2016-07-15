130451d6042ccd1dc20fe226e32bc7aaf7421761
Raphael1347 <raphael.thiemonge@gmail.com>
2016-07-15 15:26:55 -0400

    Update ColeCole.m

--- ColeCole.m
+++ ColeCole.m
@@ -1,21 +1,19 @@
-function Z = ColeCole( w )
+function Z = ColeCole( Zo,m,c,t,w )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-Zo=1000;
-
-m1=0.25;
-m2=0.75;
-c1=0.5;
-c2=1;
-t1=10.^-2;
-t2=10.^-5;
-
-z1=m1.*(1-1./(1+(1i.*w.*t1).^c1));
-
-z2=m2.*(1-1./(1+(1i.*w.*t2).^c2));
 
 
-Z=Zo.*(1-z1-z2);
+
+
+for n=1:numel(m)
+
+z(i)=m(i).*(1-1./(1+(1i.*w.*t(i)).^c(i)));
+
+end
+
+
+
+Z=Zo.*(1-sum(z))
 
 
 
